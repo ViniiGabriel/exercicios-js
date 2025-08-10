6afc32cbb6573bbf9da11275b7e688a01cd0a97e
Vinicius Gabriel <vinidomgabriel@gmail.com>
2025-08-10 16:28:59 -0300

    implementei o projectsNoIframe

--- script.js
+++ script.js
@@ -41,21 +41,25 @@ function makeCard(name) {
   aNew.href = buildUrl(name);
   aNew.target = '_blank';
 
-  // Abrir no preview (iframe)
-  const aPrev = document.createElement('button');
-  aPrev.className = 'button preview';
-  aPrev.textContent = 'Abrir no preview';
-  aPrev.addEventListener('click', () => {
-    const url = buildUrl(name);
-    preview.style.display = 'block';
-    preview.src = url;
-    preview.scrollIntoView({ behavior: 'smooth' });
-  });
-
   row.appendChild(aOpen);
   row.appendChild(aNew);
-  row.appendChild(aPrev);
   el.appendChild(row);
+
+
+  // Abrir no preview (iframe)
+  if (!projectsNoIframe(name)) {
+    const aPrev = document.createElement('button');
+    aPrev.className = 'button preview';
+    aPrev.textContent = 'Abrir no preview';
+    aPrev.addEventListener('click', () => {
+      const url = buildUrl(name);
+      preview.style.display = 'block';
+      preview.src = url;
+      preview.scrollIntoView({ behavior: 'smooth' });
+    });
+
+    row.appendChild(aPrev);
+  }
 
   return el;
 }
@@ -67,3 +71,17 @@ function buildUrl(folder) {
 }
 
 projectFolders.forEach(p => projectsDiv.appendChild(makeCard(p)));
+
+function projectsNoIframe(name) {
+  let projects = [
+    'clima-js',
+  ]
+
+  for (let projectName of projects) {
+    if (projectName == name) {
+      return true;
+    }
+  }
+
+  return false;
+}
